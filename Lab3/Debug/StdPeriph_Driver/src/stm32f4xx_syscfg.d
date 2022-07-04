e294fe92fd351c67acc84929abad3b81282ea555
omaresssam9371 <61362714+omaresssam9371@users.noreply.github.com>
2022-07-04 20:34:44 +0200

    Add files via upload

---
+++ Lab3/Debug/StdPeriph_Driver/src/stm32f4xx_syscfg.d
@@ -0,0 +1,86 @@
+StdPeriph_Driver/src/stm32f4xx_syscfg.o: \
+ ../StdPeriph_Driver/src/stm32f4xx_syscfg.c \
+ E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_syscfg.h \
+ E:/STM/Lab\ 3/CMSIS/device/stm32f4xx.h \
+ E:/STM/Lab\ 3/CMSIS/core/core_cm4.h \
+ E:/STM/Lab\ 3/CMSIS/core/core_cmInstr.h \
+ E:/STM/Lab\ 3/CMSIS/core/core_cmFunc.h \
+ E:/STM/Lab\ 3/CMSIS/core/core_cmSimd.h \
+ E:/STM/Lab\ 3/CMSIS/device/system_stm32f4xx.h \
+ E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_conf.h \
+ E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_adc.h \
+ E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_crc.h \
+ E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_dbgmcu.h \
+ E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_dma.h \
+ E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_exti.h \
+ E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_flash.h \
+ E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_gpio.h \
+ E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_i2c.h \
+ E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_iwdg.h \
+ E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_pwr.h \
+ E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_rcc.h \
+ E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_rtc.h \
+ E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_sdio.h \
+ E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_spi.h \
+ E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_syscfg.h \
+ E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_tim.h \
+ E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_usart.h \
+ E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_wwdg.h \
+ E:/STM/Lab\ 3/StdPeriph_Driver/inc/misc.h \
+ E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_rcc.h
+
+E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_syscfg.h:
+
+E:/STM/Lab\ 3/CMSIS/device/stm32f4xx.h:
+
+E:/STM/Lab\ 3/CMSIS/core/core_cm4.h:
+
+E:/STM/Lab\ 3/CMSIS/core/core_cmInstr.h:
+
+E:/STM/Lab\ 3/CMSIS/core/core_cmFunc.h:
+
+E:/STM/Lab\ 3/CMSIS/core/core_cmSimd.h:
+
+E:/STM/Lab\ 3/CMSIS/device/system_stm32f4xx.h:
+
+E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_conf.h:
+
+E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_adc.h:
+
+E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_crc.h:
+
+E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_dbgmcu.h:
+
+E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_dma.h:
+
+E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_exti.h:
+
+E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_flash.h:
+
+E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_gpio.h:
+
+E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_i2c.h:
+
+E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_iwdg.h:
+
+E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_pwr.h:
+
+E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_rcc.h:
+
+E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_rtc.h:
+
+E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_sdio.h:
+
+E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_spi.h:
+
+E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_syscfg.h:
+
+E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_tim.h:
+
+E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_usart.h:
+
+E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_wwdg.h:
+
+E:/STM/Lab\ 3/StdPeriph_Driver/inc/misc.h:
+
+E:/STM/Lab\ 3/StdPeriph_Driver/inc/stm32f4xx_rcc.h:
